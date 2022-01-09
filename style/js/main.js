d5b348a834fab8b9d657af018d5c17bf08a004e2
Akhror <40128985+t-akhror@users.noreply.github.com>
2022-01-09 14:32:24 +0900

    Update main.js

--- style/js/main.js
+++ style/js/main.js
@@ -1,152 +1,104 @@
 
-let taskList=[
-    {
-        task:'Header',
-        startDate:'06.01.2022',
-        endDate:'10.01.2022',
-        staff:'Akbar',
-        status:'pending',
-        isRejected:false
-    },
-    {
-        task:'Main',
-        startDate:'13.01.2022',
-        endDate:'25.01.2022',
-        staff:'Otabek',
-        status:'doing',
-        isRejected: false
-    },
-    {
-        task:'Navbar',
-        startDate:'08.01.2022',
-        endDate:'15.01.2022',
-        staff:'Ozodbek',
-        status:'done',
-        isRejected:false
-    },
-    {
-        task:'Footer',
-        startDate:'06.01.2022',
-        endDate:'10.01.2022',
-        staff:'Azizbek',
-        status:'rejected',
-        isRejected:true
-    },
-];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 22</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
 
-function drawTask() {
-    document.getElementById('pendingTaskList').innerHTML='';
-    document.getElementById('doingTaskList').innerHTML='';
-    document.getElementById('doneTaskList').innerHTML='';
-    document.getElementById('rejectedTaskList').innerHTML='';
-    for (let i=0; i<taskList.length; i++){
-        
-        if (taskList[i].status==='pending'){
-            document.getElementById('pendingTaskList').innerHTML +=
-                '<div class="pendingCard">'+
-                '<h6>'+ "Task: "+taskList[i].task +
-                (taskList[i].isRejected ? '<span class="badge bg-danger"> Rejected</span>' : ' ')+'</h6>'+
-                '<h6>'+ "Staff: "+taskList[i].staff +'</h6>'+
-                '<h6>'+ "Start Date: "+taskList[i].startDate +'</h6>'+
-                '<h6>'+ "End Date: "+taskList[i].endDate +'</h6>'+
-                '<select class="form-control mb-3" id="editedStatus'+i+'">' +
-                '<option selected disabled>choose</option>' +
-                '<option value="doing">doing</option>' +
-                '<option value="done">done</option>' +
-                '</select>'+
-                '<button type="button" onclick="editTask('+i+')" type="button"  class="btn btn-warning d w-50">Edit</button>'+
-                '<button type="button" onclick="deleteTask('+i+')" class="btn btn-danger d w-50">Delete</button>'+
-                '</div>'
-        }
-        else if (taskList[i].status==='doing'){
-           
-            document.getElementById('doingTaskList').innerHTML +=
-                '<div class="doingCard">'+
-                '<h6>'+ "Task: "+taskList[i].task +
-                (taskList[i].isRejected ? '<span class="badge bg-danger"> Rejected</span>' : ' ')+'</h6>'+
-                '<h6>'+ "Staff: "+taskList[i].staff +'</h6>'+
-                '<h6>'+ "Start Date: "+taskList[i].startDate +'</h6>'+
-                '<h6>'+ "End date: "+taskList[i].endDate +'</h6>'+
-                '<select class="form-control mb-3" id="editedStatus'+i+'">' +
-                '<option selected disabled>choose</option>' +
-                '<option value="pending">pending</option>' +
-                '<option value="done">done</option>' +
-                '</select>'+
-                '<button type="button" onclick="editTask('+i+')" class="btn btn-warning d w-50">Edit</button>'+
-                '<button type="button" onclick="deleteTask('+i+')" class="btn btn-danger d w-50">Delete</button>'+
-                '</div>'
-        }
-        else if (taskList[i].status==='done'){
-          
-            document.getElementById('doneTaskList').innerHTML +=
-                '<div class="doneCard">'+
-                '<h6>'+ "Task: "+taskList[i].task +
-                (taskList[i].isRejected ? '<span class="badge bg-danger"> Rejected</span>' : ' ')+'</h6>'+
-                '<h6>'+ "Staff: "+taskList[i].staff +'</h6>'+
-                '<h6>'+ "Start Date: "+taskList[i].startDate +'</h6>'+
-                '<h6>'+ "End Date: "+taskList[i].endDate +'</h6>'+
-                '<button type="button" onclick="rejectTask('+i+')"class="btn btn-danger d w-75">Rejected</button>'+
-                '</div>'
-        }
-        else{
+<div class="container-fluid">
+    <div class="row my-3">
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-dark text-white text-center">
+                    <h4>Add task</h4>
+                </div>
+                <div class="card-body">
+                    <form name="myForm">
+                        <label for="task">Taskni kiriting</label>
+                        <textarea class="form-control mb-2" name="task" id="task" 
+                                  cols="30" rows="3"></textarea>
+
+                        <label for="startDate">Boshlanish vaqti</label>
+                        <input type="date" id="startDate" name="starDate" class="form-control mb-2">
+
+
+                        <label for="endDate">Tugash vaqti</label>
+                        <input type="date" id="endDate" name="endDate" class="form-control mb-2">
+
+                        <label for="staff">Xodimni tanlang</label>
+                        <select class="form-control mb-2" name="staff" id="staff">
+                            <option selected disabled>choose</option>
+                            <option value="muhammad">Muhammad</option>
+                            <option value="axror">Axror</option>
+                            <option value="doston">Doston</option>
+                        </select>
+
+                        <label for="status">Statusni tanlang</label>
+                        <select class="form-control mb-2" name="status" id="status">
+                            <option selected disabled>choose</option>
+                            <option value="pending">pending</option>
+                            <option value="doing">doing</option>
+                        </select>
+                        <button type="button" onclick="addTask()" class="btn btn-dark mt-3 d-block w-100">
+                            add task
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-warning text-center text-white">
+                    <h4>Pending task</h4>
+                </div>
+            </div>
+                <div class="" id="pendingTaskList">
+                  
+                </div>
             
-            document.getElementById('rejectedTaskList').innerHTML +=
-                '<div class="rejectedCard">'+
-                '<h6>'+ "Task: "+taskList[i].task +'</h6>'+
-                '<h6>'+ "Staff: "+taskList[i].staff +'</h6>'+
-                '<h6>'+ "Start Date: "+taskList[i].startDate +'</h6>'+
-                '<h6>'+ "End Date: "+taskList[i].endDate +'</h6>'+
-                '<select class="form-control mb-3" id="editedStatus'+i+'">' +
-                '<option selected disabled>choose</option>' +
-                '<option value="pending">pending</option>' +
-                '<option value="doing">doing</option>' +
-                '</select>'+
-                '<button type="button" onclick="editTask('+i+')" class="btn btn-warning d w-50">Edit</button>'+
-                '</div>'
-        }
-    }
-}
-drawTask();
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-primary text-center text-white">
+                    <h4>Doing task</h4>
+                </div>
+            </div>
+            
+            <div id="doingTaskList">
 
-function addTask(){
-    let task= document.forms['myForm']['task'].value;
-    let startDate=document.forms['myForm']['startDate'].value;
-    let endDate = document.forms['myForm']['endDate'].value;
-    let staff=document.forms['myForm']['staff'].value;
-    let status=document.forms['myForm']['status'].value;
-    if (task.trim().length>0 && startDate.trim().length>0 
-        && endDate.trim().length>0 && staff !='choose' && status !='choose' ){
-            let newTask={
-                task, 
-                startDate, 
-                endDate, 
-                staff,
-                status,
-                isRejected:false 
-            };
-            taskList.push(newTask);
-            drawTask();
-            document.forms['myForm'].reset();
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-success text-center text-white">
+                    <h4>Done task</h4>
+                </div>
+            </div>
+            <div  id="doneTaskList">
 
-    }else{
-        alert("Fill all fields!");
-    }
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-danger text-center text-white">
+                    <h4>Rejected task</h4>
+                </div>
+            </div>
+            
+            <div id="rejectedTaskList">
 
-}
+            </div>
+        </div>
+    </div>
+</div>
 
-function deleteTask(taskIndex){
-    taskList.splice(taskIndex,1);
-    drawTask();
-}
-function rejectTask(taskIndex){
-    taskList[taskIndex].status="rejected";
-    taskList[taskIndex].isRejected=true;
-    drawTask();
-}
-function editTask(editIndex){
-    let selectedStatus=document.getElementById("editedStatus"+editIndex).value;
-   if(selectedStatus!='choose'){
-       taskList[editIndex].status=selectedStatus;
-       drawTask();
-   }else alert('Choose status!');
-}
+
+
+<script src="js/bootstrap.bundle.js"></script>
+<script src="index.js"></script>
+</body>
+</html>
